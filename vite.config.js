b14581d13c67d78d7914169673c79bdce8946676
unknown <nazirkhan.brj@gmail.com>
2024-11-27 16:05:52 +0545

    supabase configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config.env" }); // Load the custom config.env file
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
